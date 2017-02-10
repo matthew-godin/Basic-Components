c1f311e1d95eba62b764ba8c6fb9ab99656361b2
Matthew Godin <matthew.godin.software.engineer@gmail.com>
2017-02-10 12:26:00 -0500

    added Tile TexturedTile and ColoredTile

---
+++ BasicComponents/ColoredTile.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace XNAProject
+{
+   public class ColoredTile : Tile
+   {
+      const int NUM_TRIANGLES = 2;
+      VertexPositionColor[] Vertices { get; set; }
+      Color Color { get; set; }
+
+      public ColoredTile(Game game, float initialScale, Vector3 initialRotation, Vector3 initialPosition, 
+                          Vector2 span, Color color, float updateInterval)
+         : base(game, initialScale, initialRotation, initialPosition, span, updateInterval)
+      {
+         Color = color;
+      }
+
+      protected override void CreateVertexArray()
+      {
+         Vertices = new VertexPositionColor[NumVertices];
+      }
+
+      protected override void InitializeBscEffectParameters()
+      {
+         BscEffect.VertexColorEnabled = true;
+      }
+
+      protected override void InitializeVertices() // Is called by base.Initialize()
+      {
+         int vertexIndex = -1;
+         for (int j = 0; j < 1; ++j)
+         {
+            for (int i = 0; i < 2; ++i)
+            {
+               Vertices[++vertexIndex] = new VertexPositionColor(VertexPts[i, j], Color);
+               Vertices[++vertexIndex] = new VertexPositionColor(VertexPts[i, j + 1], Color);
+            }
+         }
+      }
+
+      protected override void DrawTriangleStrip()
+      {
+         GraphicsDevice.DrawUserPrimitives<VertexPositionColor>(PrimitiveType.TriangleStrip, Vertices, 0, NUM_TRIANGLES);
+      }
+   }
+}
+
---
+++ BasicComponents/TexturedTile.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace XNAProject
+{
+   public class TexturedTile : Tile
+   {
+      const int NUM_TRIANGLES = 2;
+      ResourceManager<Texture2D> TextureMgr;
+      Texture2D textureTile;
+      GreenxPositionTexture[] Vertices { get; set; }
+      Vector2[,] TexturePts { get; set; }
+      string TextureNameTile { get; set; }
+      BlendState AlphaMgr { get; set; }
+
+      public TexturedTile(Game game, float initialScale, Vector3 initialRotation, Vector3 initialPosition, Vector2 span, string textureNameTile, float updateInterval)
+         : base(game, initialScale, initialRotation, initialPosition, span, updateInterval)
+      {
+         TextureNameTile = textureNameTile;
+      }
+
+      protected override void CreateVertexArray()
+      {
+         TexturePts = new Vector2[2, 2];
+         Vertices = new GreenxPositionTexture[NumVertices];
+         CreateTexturePointArray();
+      }
+
+      private void CreateTexturePointArray()
+      {
+         TexturePts[0, 0] = new Vector2(0, 1);
+         TexturePts[1, 0] = new Vector2(1, 1);
+         TexturePts[0, 1] = new Vector2(0, 0);
+         TexturePts[1, 1] = new Vector2(1, 0);
+      }
+
+      protected override void InitializeVertices() // Is called by base.Initialize()
+      {
+         int vertexIndex = -1;
+         for (int j = 0; j < 1; ++j)
+         {
+            for (int i = 0; i < 2; ++i)
+            {
+               Vertices[++vertexIndex] = new GreenxPositionTexture(VertexPts[i, j], TexturePts[i, j]);
+               Vertices[++vertexIndex] = new GreenxPositionTexture(VertexPts[i, j + 1], TexturePts[i, j + 1]);
+            }
+         }
+      }
+
+      protected override void LoadContent()
+      {
+
+         TextureMgr = Game.Services.GetService(typeof(ResourceManager<Texture2D>)) as ResourceManager<Texture2D>;
+         textureTile = TextureMgr.Find(TextureNameTile);
+         base.LoadContent();
+      }
+
+      protected override void InitializeBscEffectParameters()
+      {
+         BscEffect.TextureEnabled = true;
+         BscEffect.Texture = textureTile;
+         AlphaMgr = BlendState.AlphaBlend; // Attention à ceci...
+      }
+
+      public override void Draw(GameTime gameTime)
+      {
+         BlendState oldBlendState = GraphicsDevice.BlendState; // ... and à cela!
+         GraphicsDevice.BlendState = AlphaMgr;
+         base.Draw(gameTime);
+         GraphicsDevice.BlendState = oldBlendState;
+      }
+
+      protected override void DrawTriangleStrip()
+      {
+         GraphicsDevice.DrawUserPrimitives<GreenxPositionTexture>(PrimitiveType.TriangleStrip, Vertices, 0, NUM_TRIANGLES);
+      }
+   }
+}
