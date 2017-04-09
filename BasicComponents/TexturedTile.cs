9918db5dfa884ef6aac13e6b262bc45b33fce434
Matthew Godin <matthew.godin.software.engineer@gmail.com>
2017-04-09 18:29:46 -0400

    Tile and TexturedTile back to normal

--- BasicComponents/TexturedTile.cs
+++ BasicComponents/TexturedTile.cs
@@ -5,76 +5,77 @@ using Microsoft.Xna.Framework;
 
 namespace XNAProject
 {
-   public class TexturedTile : Tile
-   {
-      const int NUM_TRIANGLES = 2;
-      ResourceManager<Texture2D> TextureMgr;
-      protected Texture2D Texture { get; set; }  //Texture2D textureTile;
-      GreenxPositionTexture[] Vertices { get; set; }
-      protected Vector2[,] TexturePts { get; set; }
-      string TextureNameTile { get; set; }
-      BlendState AlphaMgr { get; set; }
+    public class TexturedTile : Tile
+    {
+        const int NUM_TRIANGLES = 2;
+        ResourceManager<Texture2D> TextureMgr;
+        protected Texture2D Texture { get; set; }  //Texture2D textureTile;
+        GreenxPositionTexture[] Vertices { get; set; }
+        protected Vector2[,] TexturePts { get; set; }
+        string TextureNameTile { get; set; }
+        BlendState AlphaMgr { get; set; }
 
-      public TexturedTile(Game game, float initialScale, Vector3 initialRotation, Vector3 initialPosition, Vector2 span, string textureNameTile, float updateInterval)
-         : base(game, initialScale, initialRotation, initialPosition, span, updateInterval)
-      {
-         TextureNameTile = textureNameTile;
-      }
+        public TexturedTile(Game game, float initialScale, Vector3 initialRotation, Vector3 initialPosition, Vector2 span, string textureNameTile/*, float updateInterval*/)
+           : base(game, initialScale, initialRotation, initialPosition, span/*, updateInterval*/)
+        {
+            TextureNameTile = textureNameTile;
+        }
 
-      protected override void CreateVertexArray()
-      {
-         TexturePts = new Vector2[2, 2];
-         Vertices = new GreenxPositionTexture[NumVertices];
-         CreateTexturePointArray();
-      }
+        protected override void CreateVertexArray()
+        {
+            TexturePts = new Vector2[2, 2];
+            Vertices = new GreenxPositionTexture[NumVertices];
+            CreateTexturePointArray();
+        }
 
-      private void CreateTexturePointArray()
-      {
-         TexturePts[0, 0] = new Vector2(0, 1);
-         TexturePts[1, 0] = new Vector2(1, 1);
-         TexturePts[0, 1] = new Vector2(0, 0);
-         TexturePts[1, 1] = new Vector2(1, 0);
-      }
+        private void CreateTexturePointArray()
+        {
+            TexturePts[0, 0] = new Vector2(0, 1);
+            TexturePts[1, 0] = new Vector2(1, 1);
+            TexturePts[0, 1] = new Vector2(0, 0);
+            TexturePts[1, 1] = new Vector2(1, 0);
+        }
 
-      protected override void InitializeVertices() // Is called by base.Initialize()
-      {
-         int vertexIndex = -1;
-         for (int j = 0; j < 1; ++j)
-         {
-            for (int i = 0; i < 2; ++i)
+        protected override void InitializeVertices() // Is called by base.Initialize()
+        {
+            int vertexIndex = -1;
+            for (int j = 0; j < 1; ++j)
             {
-               Vertices[++vertexIndex] = new GreenxPositionTexture(VertexPts[i, j], TexturePts[i, j]);
-               Vertices[++vertexIndex] = new GreenxPositionTexture(VertexPts[i, j + 1], TexturePts[i, j + 1]);
+                for (int i = 0; i < 2; ++i)
+                {
+                    Vertices[++vertexIndex] = new GreenxPositionTexture(VertexPts[i, j], TexturePts[i, j]);
+                    Vertices[++vertexIndex] = new GreenxPositionTexture(VertexPts[i, j + 1], TexturePts[i, j + 1]);
+                }
             }
-         }
-      }
+        }
 
-      protected override void LoadContent()
-      {
+        protected override void LoadContent()
+        {
 
-         TextureMgr = Game.Services.GetService(typeof(ResourceManager<Texture2D>)) as ResourceManager<Texture2D>;
-         /*textureTile*/Texture = TextureMgr.Find(TextureNameTile);
-         base.LoadContent();
-      }
+            TextureMgr = Game.Services.GetService(typeof(ResourceManager<Texture2D>)) as ResourceManager<Texture2D>;
+            /*textureTile*/
+            Texture = TextureMgr.Find(TextureNameTile);
+            base.LoadContent();
+        }
 
-      protected override void InitializeBscEffectParameters()
-      {
-         BscEffect.TextureEnabled = true;
-         BscEffect.Texture = /*textureTile*/Texture;
-         AlphaMgr = BlendState.AlphaBlend; // Attention à ceci...
-      }
+        protected override void InitializeBscEffectParameters()
+        {
+            BscEffect.TextureEnabled = true;
+            BscEffect.Texture = /*textureTile*/Texture;
+            AlphaMgr = BlendState.AlphaBlend; // Attention à ceci...
+        }
 
-      public override void Draw(GameTime gameTime)
-      {
-         BlendState oldBlendState = GraphicsDevice.BlendState; // ... and à cela!
-         GraphicsDevice.BlendState = AlphaMgr;
-         base.Draw(gameTime);
-         GraphicsDevice.BlendState = oldBlendState;
-      }
+        public override void Draw(GameTime gameTime)
+        {
+            BlendState oldBlendState = GraphicsDevice.BlendState; // ... and à cela!
+            GraphicsDevice.BlendState = AlphaMgr;
+            base.Draw(gameTime);
+            GraphicsDevice.BlendState = oldBlendState;
+        }
 
-      protected override void DrawTriangleStrip()
-      {
-         GraphicsDevice.DrawUserPrimitives<GreenxPositionTexture>(PrimitiveType.TriangleStrip, Vertices, 0, NUM_TRIANGLES);
-      }
-   }
+        protected override void DrawTriangleStrip()
+        {
+            GraphicsDevice.DrawUserPrimitives<GreenxPositionTexture>(PrimitiveType.TriangleStrip, Vertices, 0, NUM_TRIANGLES);
+        }
+    }
 }
--- BasicComponents/Tile.cs
+++ BasicComponents/Tile.cs
@@ -5,64 +5,64 @@ using Microsoft.Xna.Framework;
 
 namespace XNAProject
 {
-   public abstract class Tile : AnimatedBasicPrimitive
-   {
-      const int NUM_TRIANGLES = 2;
-      protected Vector3[,] VertexPts { get; private set; }
-      Vector3 Origin { get; set; }
-      Vector2 Delta { get; set; }
-      protected BasicEffect BscEffect { get; private set; }
+    public abstract class Tile : BasicPrimitive//AnimatedBasicPrimitive
+    {
+        const int NUM_TRIANGLES = 2;
+        protected Vector3[,] VertexPts { get; private set; }
+        Vector3 Origin { get; set; }
+        Vector2 Delta { get; set; }
+        protected BasicEffect BscEffect { get; private set; }
 
 
-      public Tile(Game game, float initialScale, Vector3 initialRotation, Vector3 initialPosition, 
-                   Vector2 span, float updateInterval)
-         : base(game, initialScale, initialRotation, initialPosition, updateInterval)
-      {
-         Delta = new Vector2(span.X, span.Y);
-         Origin = new Vector3(-Delta.X / 2, -Delta.Y / 2, 0); //to center the primitive to point (0,0,0)
-      }
+        public Tile(Game game, float initialScale, Vector3 initialRotation, Vector3 initialPosition,
+                     Vector2 span/*, float updateInterval*/)
+           : base(game, initialScale, initialRotation, initialPosition/*, updateInterval*/)
+        {
+            Delta = new Vector2(span.X, span.Y);
+            Origin = new Vector3(-Delta.X / 2, -Delta.Y / 2, 0); //to center the primitive to point (0,0,0)
+        }
 
-      public override void Initialize()
-      {
-         NumVertices = NUM_TRIANGLES + 2;
-         VertexPts = new Vector3[2, 2];
-         CreatePointArray();
-         CreateVertexArray();
-         base.Initialize();
-      }
+        public override void Initialize()
+        {
+            NumVertices = NUM_TRIANGLES + 2;
+            VertexPts = new Vector3[2, 2];
+            CreatePointArray();
+            CreateVertexArray();
+            base.Initialize();
+        }
 
-      private void CreatePointArray()
-      {
-         VertexPts[0, 0] = new Vector3(Origin.X, Origin.Y, Origin.Z);
-         VertexPts[1, 0] = new Vector3(Origin.X + Delta.X, Origin.Y, Origin.Z);
-         VertexPts[0, 1] = new Vector3(Origin.X, Origin.Y + Delta.Y, Origin.Z);
-         VertexPts[1, 1] = new Vector3(Origin.X + Delta.X, Origin.Y + Delta.Y, Origin.Z);
-      }
+        private void CreatePointArray()
+        {
+            VertexPts[0, 0] = new Vector3(Origin.X, Origin.Y, Origin.Z);
+            VertexPts[1, 0] = new Vector3(Origin.X + Delta.X, Origin.Y, Origin.Z);
+            VertexPts[0, 1] = new Vector3(Origin.X, Origin.Y + Delta.Y, Origin.Z);
+            VertexPts[1, 1] = new Vector3(Origin.X + Delta.X, Origin.Y + Delta.Y, Origin.Z);
+        }
 
-      protected abstract void CreateVertexArray();
+        protected abstract void CreateVertexArray();
 
-      protected override void LoadContent()
-      {
-         BscEffect = new BasicEffect(GraphicsDevice);
-         InitializeBscEffectParameters();
-         base.LoadContent();
-      }
+        protected override void LoadContent()
+        {
+            BscEffect = new BasicEffect(GraphicsDevice);
+            InitializeBscEffectParameters();
+            base.LoadContent();
+        }
 
-      protected abstract void InitializeBscEffectParameters();
+        protected abstract void InitializeBscEffectParameters();
 
-      public override void Draw(GameTime gameTime)
-      {
-         BscEffect.World = GetWorld();
-         BscEffect.View = GameCamera.View;
-         BscEffect.Projection = GameCamera.Projection;
-         foreach (EffectPass passEffect in BscEffect.CurrentTechnique.Passes)
-         {
-            passEffect.Apply();
-            DrawTriangleStrip();
-         }
-      }
+        public override void Draw(GameTime gameTime)
+        {
+            BscEffect.World = GetWorld();
+            BscEffect.View = GameCamera.View;
+            BscEffect.Projection = GameCamera.Projection;
+            foreach (EffectPass passEffect in BscEffect.CurrentTechnique.Passes)
+            {
+                passEffect.Apply();
+                DrawTriangleStrip();
+            }
+        }
 
-      protected abstract void DrawTriangleStrip();
-   }
+        protected abstract void DrawTriangleStrip();
+    }
 }
 
